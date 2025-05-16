f924af6eb37d17e8471ffade0910c03628cf91dd
Your Name <you@example.com>
2025-05-16 18:55:40 +0800

    acc

--- core/llava_eval.py
+++ core/llava_eval.py
@@ -12,14 +12,14 @@ from PIL import Image
 # python llava_eval.py --anno_path ~/SEED-Bench-2/SEED-Bench_v2_level1_2_3.json 
 
 # root directory of cc3m
-cc3m_dir = "/home/dt/SEED-Bench-2/cc3m-image"
+cc3m_dir = "/home/buaa/dengtao/dataset/SEED-Bench-2/cc3m"
 # root directory of seed bench v2
 seed_bench_v2_dir = "/home/dt/SEED-Bench-2/seed_bench_image_v2"
 # 选择第二张 GPU
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # 模型路径
-MODEL_PATH = "/home/dt/llava-1.5-7b-hf"
+MODEL_PATH = "/home/buaa/dengtao/model/llava-v1.5-7b"
 
 seed = 0
 
@@ -65,10 +65,16 @@ def build_model(model_name):
     if model_name == 'llava_1.5':
         # 直接加载本地的llava模型和tokenizer
         # 加载模型和 processor
-        from transformers import LlavaForConditionalGeneration, LlavaProcessor
-        processor = LlavaProcessor.from_pretrained(MODEL_PATH, use_fast=True)
-        model = LlavaForConditionalGeneration.from_pretrained(MODEL_PATH, torch_dtype=torch.float16).to(device)
-        return model, processor
+        from llava.model.builder import load_pretrained_model
+        tokenizer, model, processor, context_len = load_pretrained_model(
+            model_path=MODEL_PATH,
+            model_base=None,
+            model_name=model_name,
+            device_map=None,  # 禁用自动设备映射
+        )
+        model = model.to(device)
+        model.eval()
+        return model, processor, tokenizer
     else:
         raise ValueError(f"Unsupported model: {model_name}")
 
@@ -79,7 +85,8 @@ def is_integer_string(s):
     except ValueError:
         return False
 
-def run_inference(model, processor, qa_anno, output_dir):
+def run_inference(model, processor, tokenizer, qa_anno, output_dir):
+    from llava.mm_utils import process_images, tokenizer_image_token
     total_qa_num = len(qa_anno)
     answer_list = []
     output_f = open(os.path.join(output_dir, "results.json"), "a")
@@ -102,18 +109,25 @@ def run_inference(model, processor, qa_anno, output_dir):
             image_path = image_dir + '/' + qa_item['data_id']
             image = Image.open(image_path).convert("RGB")
             # 将图像和问题转换为模型可以理解的输入
-            inputs = processor(images=image, text=question, return_tensors="pt", do_resize=True).to(device)
+            image_tensor = process_images([image], processor, model.config).to(model.device, dtype=torch.float16)
+            input_ids = tokenizer_image_token(question, tokenizer, -200, return_tensors='pt').unsqueeze(0).cuda()
 
             # 生成回答
-            with torch.no_grad():
-                output = model.generate(**inputs, max_new_tokens=128)
+            output_ids = model.generate(
+                    input_ids,
+                    images=image_tensor,
+                    image_sizes=image.size,
+                    do_sample=True,
+                    # no_repeat_ngram_size=3,
+                    max_new_tokens=512,
+                    use_cache=True
+                )
 
             # 解码输出
-            response = processor.batch_decode(output, skip_special_tokens=True)[0]
+            response = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
             # print(image_path)
-            # print(response)
             # 从生成的答案中选择正确的选项
-            response = response[len(question)-1:]
+            response = response.replace('Answer', '')
             match = re.search(r'[ABCD]', response)
             pred_id = match.group()
             
@@ -129,7 +143,8 @@ def run_inference(model, processor, qa_anno, output_dir):
             answer_list.append(answer_record)
             # output prediction record for each question
             output_f.write(json.dumps(answer_record) + "\n")
-        except:
+        except Exception as e:
+            print(f"Error processing question {qa_item['question_id']}: {e}")
             pass
         step += 1
     print('acc:' + str(correct_num/all_num))
@@ -139,7 +154,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Arg Parser')
     parser.add_argument('--model', type=str, default='llava_1.5')  # 本地llava模型
-    parser.add_argument('--anno_path', type=str, default='SEED-Bench_v2_level1_2_3.json')
+    parser.add_argument('--anno_path', type=str, default='/home/buaa/dengtao/dataset/SEED-Bench-2/SEED-Bench_v2_level1_2_3.json')
     parser.add_argument('--output_dir', type=str, default='results')
     parser.add_argument('--evaluate_level', type=str, default='L2')
     parser.add_argument('--evaluate_part', type=str, default='all')
@@ -156,6 +171,6 @@ if __name__ == '__main__':
 
     print(f'evaluating.. {args.model}')
     # 直接加载模型和tokenizer
-    model, processor = build_model(args.model)
+    model, processor, tokenizer = build_model(args.model)
 
-    run_inference(model, processor, qa_anno, args.output_dir)
+    run_inference(model, processor, tokenizer, qa_anno, args.output_dir)
